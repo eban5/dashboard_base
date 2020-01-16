c8488a61921d954835b1eb7d6221fe8af8951782
Esteban Amas <eeamas@pbs.org>
2020-01-15 21:49:06 -0500

    Base files. init commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Avatar } from "./avatar.js";
+import "antd/dist/antd.css";
+import "./App.css";
+
+import { Layout, Menu, Breadcrumb, Icon } from "antd";
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+const UserAvatar = props => {
+	const { user, size } = props;
+	let width = size === "small" ? "100px" : "500px";
+
+	return (
+		<div className="user_avatar">
+			<div style={{ width: "80%", display: "block", margin: "0 auto" }}>
+				<Avatar />
+				<div style={{ color: "white", margin: "16px 0" }}>
+					<strong> {user}</strong>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [collapsed, setCollapsed] = useState(false);
+	const user = "Joe Cool";
+	const copyRightYear = new Date().getFullYear();
+
+	const onCollapse = collapsed => {
+		console.log(collapsed);
+		setCollapsed(collapsed);
+	};
+
+	return (
+		<div className="App">
+			<Layout style={{ minHeight: "100vh" }}>
+				<Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+					<UserAvatar size="small" user={user} />
+					<Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+						<Menu.Item key="1">
+							<Icon type="pie-chart" />
+							<span>Option 1</span>
+						</Menu.Item>
+						<Menu.Item key="2">
+							<Icon type="desktop" />
+							<span>Option 2</span>
+						</Menu.Item>
+						<SubMenu
+							key="sub1"
+							title={
+								<span>
+									<Icon type="user" />
+									<span>User</span>
+								</span>
+							}>
+							<Menu.Item key="3">Tom</Menu.Item>
+							<Menu.Item key="4">Bill</Menu.Item>
+							<Menu.Item key="5">Alex</Menu.Item>
+						</SubMenu>
+						<SubMenu
+							key="sub2"
+							title={
+								<span>
+									<Icon type="team" />
+									<span>Team</span>
+								</span>
+							}>
+							<Menu.Item key="6">Team 1</Menu.Item>
+							<Menu.Item key="8">Team 2</Menu.Item>
+						</SubMenu>
+						<Menu.Item key="9">
+							<Icon type="file" />
+							<span>File</span>
+						</Menu.Item>
+					</Menu>
+				</Sider>
+				<Layout>
+					<Header style={{ background: "#fff", padding: 0 }} />
+					<Content style={{ margin: "0 16px" }}>
+						<Breadcrumb style={{ margin: "16px 0" }}>
+							<Breadcrumb.Item>User</Breadcrumb.Item>
+							<Breadcrumb.Item>Bill</Breadcrumb.Item>
+						</Breadcrumb>
+						<div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
+							{user}'s Profile
+						</div>
+					</Content>
+					<Footer style={{ textAlign: "center" }}>
+						Esteban Amas ©{copyRightYear}. Made with Ant Design.
+					</Footer>
+				</Layout>
+			</Layout>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/avatar.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+export const Avatar = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 20 20"
+			version="1.1"
+			shapeRendering="crispEdges">
+			<path
+				d="M8 15v1H4v1H3v3h14v-3h-1v-1h-4v-1h3v-1h1v-1h1v-3h1V7h-1V4h-1V3h-1V2H5v1H4v1H3v3H2v3h1v3h1v1h1v1h3z"
+				fill="#CB9E6E"
+			/>
+			<path
+				d="M5 15v-1H4v-1H3v-3H2V7h1V4h1V3h1V2h10v1h1v1h1v3h1v3h-1v3h-1v1h-1v1H5z"
+				fill="#FFF"
+				fillOpacity=".1"
+			/>
+			<path d="M5 7h3v2H5V7zm7 0h3v2h-3V7z" fill="#FFF" />
+			<path d="M7 8h1v1H7V8zm5 0h1v1h-1V8z" fill="#5b7c8b" />
+			<path d="M6 5h3v2H8V6H6V5zm5 0h3v1h-2v1h-1V5z" fillRule="evenodd" fill="#000000" />
+			<path d="M8 13h3v1H8v-1z" fill="#eec1ad" />
+			<path d="M5 8h3v1H5V8zm7 0h3v1h-3V8z" fillRule="evenodd" fill="#FFF" fillOpacity=".2" />
+			<path d="M7 8h1v1H7V8zm7 0h1v1h-1V8z" fillRule="evenodd" fill="#FFF" fillOpacity=".2" />
+			<path
+				d="M5 8v1h3V8H5zM3 7v1h1v1h1v1h3V9h1V8h2v1h1v1h3V9h1V8h1V7H3zm9 1v1h3V8h-3z"
+				fillRule="evenodd"
+				fill="#5f705c"
+			/>
+			<path
+				d="M3 7v1h1V7H3zm6 0v1h2V7H9zm7 0v1h1V7h-1z"
+				fillRule="evenodd"
+				fill="#FFF"
+				fillOpacity=".2"
+			/>
+			<path d="M5 16H4v1H3v3h14v-3h-1v-1h-3v1H7v-1H5z" fill="#03396c" />
+			<path d="M10 20v-1h3v1h-3z" fill="#FFF" fillOpacity=".5" />
+			<path
+				d="M5 16H4v1H3v3h1v-1h1v-3zm1 0h1v1h6v-1h1v2H6v-2zm9 0h1v1h1v3h-1v-1h-1v-3z"
+				fillRule="evenodd"
+				fill="#FFF"
+				fillOpacity=".8"
+			/>
+			<path d="M4 4h12V3h-1V2H5v1H4v1z" fill="#090806" />
+			<path d="M2 4V2h2V0h12v2h2v2H2z" fill="#18293b" />
+			<path d="M4 0v2h12V0H4z" fill="#FFF" fillOpacity=".2" />
+		</svg>
+	);
+};
