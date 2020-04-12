3ce04e1512d155fa208dcd9c4d483c0401e476d0
Esteban Amas <eeamas@pbs.org>
2020-04-12 13:43:22 -0400

    styling download button

--- src/Chart.js
+++ src/Chart.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
 import { getProblemNames, getProblemNameResults } from "./utils";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
 export function RPMChart(props) {
 	const { results } = props;
@@ -40,59 +42,65 @@ export function RPMChart(props) {
 	};
 
 	return (
-		<div>
-			<a
-				onClick={() => {
-					downloadChart();
-				}}
-				id="download"
-				download="ChartImage.png"
-				href=""
-				title="Download Chart">
-				Download
-			</a>
-			<Bar
-				ref={chartRef}
-				data={datasetState}
-				options={{
-					title: {
-						display: true,
-						text: `${datasetState.title} Scores`,
-						fontSize: 17,
-						fontFamily: "Palatino",
-					},
-					legend: {
-						display: true,
-						position: "right",
-						fontFamily: "Palatino",
-					},
-					scales: {
-						yAxes: [
-							{
-								scaleLabel: {
-									display: true,
-									labelString: "# Correct",
-									fontFamily: "Palatino",
+		<>
+			<div className="download">
+				<Button type="primary" shape="round" size={"large"}>
+					<a
+						onClick={() => {
+							downloadChart();
+						}}
+						id="download"
+						download="ChartImage.png"
+						href=""
+						title="Download Chart">
+						Download
+					</a>
+				</Button>
+			</div>
+			<div className="chart">
+				<Bar
+					ref={chartRef}
+					data={datasetState}
+					options={{
+						title: {
+							display: true,
+							text: `${datasetState.title} Scores`,
+							fontSize: 17,
+							fontFamily: "Palatino",
+						},
+						legend: {
+							display: true,
+							position: "right",
+							fontFamily: "Palatino",
+						},
+						scales: {
+							yAxes: [
+								{
+									scaleLabel: {
+										display: true,
+										labelString: "# Correct",
+										fontFamily: "Palatino",
+									},
+									ticks: {
+										fontFamily: "Palatino",
+										suggestedMin: 0,
+										suggestedMax: 12,
+									},
 								},
-								ticks: {
-									fontFamily: "Palatino",
-									suggestedMin: 0,
-									suggestedMax: 12,
+							],
+							xAxes: [
+								{
+									scaleLabel: {
+										display: true,
+										labelString: "Submission",
+										fontFamily: "Palatino",
+									},
 								},
-							},
-						],
-						xAxes: [
-							{
-								scaleLabel: {
-									display: true,
-									labelString: "Submission",
-									fontFamily: "Palatino",
-								},
-							},
-						],
-					},
-				}}
-			/>
-		</div>
+							],
+						},
+					}}
+				/>
+			</div>
+		</>
 	);
 }
